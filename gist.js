2b9e8a0503060b95fe4626ae337f3b90cc2325bc
Faris Kurnia Ardi <faris.ardi@modalrakyat.id>
2019-06-29 10:57:56 +0700

    shoppe test

---
+++ gist.js
@@ -0,0 +1,42 @@
+import { Selector } from 'testcafe';
+import Page from './model';
+import config from './config';
+
+const page = new Page();
+
+fixture `Fixture`
+    .page `${config.baseUrl}`;
+
+test('create', async t => {
+    await t
+    .maximizeWindow()
+    .setTestSpeed(0.5)
+    .typeText(page.inputEmail, 'depfaris@gmail.com')
+    .typeText(page.inputPass, 'Cermat123')
+    .click(page.buttonLogin)
+    .navigateTo('https://gist.github.com/')
+    .typeText(page.inputBody, 'f')
+    .click(page.public)
+});
+test('see all', async t => {
+    await t 
+    .maximizeWindow()
+    .typeText(page.inputEmail, 'depfaris@gmail.com')
+    .typeText(page.inputPass, 'Cermat123')
+    .click(page.buttonLogin)
+    .navigateTo('https://gist.github.com/')
+    .click(page.seeAll)
+   
+});
+test('delete', async t => {
+    await t 
+    .maximizeWindow()
+    .typeText(page.inputEmail, 'depfaris@gmail.com')
+    .typeText(page.inputPass, 'Cermat123')
+    .click(page.buttonLogin)
+    .navigateTo('https://gist.github.com/')
+    .click(page.pilihGist)
+    .click(page.buttonDelete)
+   
+});
+
---
+++ model.js
@@ -0,0 +1,20 @@
+import { Selector } from 'testcafe';
+
+export default class Page {
+    constructor () {
+        //Happy Scenario or Normal Case
+        this.inputEmail = Selector('#login_field');
+        this.inputPass = Selector('#password');
+        this.buttonLogin = Selector('.btn-primary');
+        this.inputBody  = Selector('pre');
+        this.public = Selector('#new_gist > div > div.form-actions > button:nth-child(1)');
+        this.pilihGist = Selector('a').withText('gist:636e01e0e0cc694082f7bd3fa6836c44')
+        this.seeAll = Selector('a').withText('See all of your gists');
+        this.buttonDelete = Selector('//*[@id="gist-pjax-container"]/div[1]/div/div[1]/ul/li[2]/form/button')
+        
+       
+
+
+    }
+
+}
